2046b39c48d168760530aa7341ab0980f31a298c
mmanvelyanknuua <mmanvelyan@knu.ua>
2022-09-28 22:48:08 +0300

    2

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,17 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Click += button1_Click;
+        }
+
+        private void button1_Click(object? sender, EventArgs e)
+        {
+            MessageBox.Show($"Ви ввели число {textBox1.Text}");
+        }
+    }
+}
